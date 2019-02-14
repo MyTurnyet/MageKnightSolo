7d2e52c1cf6288e279f079be3125e33aab88b9b5
Paige Watson <paige.watson@outlook.com>
2019-02-14 13:37:13 -0800

    r - refactor to alphabetize
    * fix issue with New Random().Next()

--- Solo/Models/DummyDeck.cs
+++ Solo/Models/DummyDeck.cs
@@ -8,17 +8,27 @@ namespace Solo.Models
     {
         private readonly string[] _currentDeck;
 
-        public DummyDeck(): this(new []{"r","r","r","r","g","g","g","g","b","b","b","b","w","w","w","w"}){}
+        public DummyDeck() : this(
+            new[] {"r", "r", "r", "r", "g", "g", "g", "g", "b", "b", "b", "b", "w", "w", "w", "w"})
+        {
+        }
 
         public DummyDeck(string[] currentDeck)
         {
             _currentDeck = currentDeck;
         }
+
         public int CardCount() => _currentDeck.Length;
-        public IDeck Shuffle() => new DummyDeck(_currentDeck.OrderBy(card=> new Random().Next()).ToArray());
 
         public override bool Equals(object obj) => obj is DummyDeck deck && Equals(deck);
         private bool Equals(DummyDeck other) => _currentDeck.SequenceEqual(other._currentDeck);
         public override int GetHashCode() => (_currentDeck != null ? _currentDeck.GetHashCode() : 0);
+
+        public IDeck Shuffle()
+        {
+            Random random = new Random();
+            return new DummyDeck(_currentDeck.OrderBy(card => random.Next()).ToArray());
+        }
+
     }
 }
